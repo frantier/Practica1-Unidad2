9208cabb9160d658303d9e3010037ba281ba5e45
PeterParker <pedro_ruvalcaba@hotmail.com>
2021-10-16 14:34:55 -0500

    Datos de la Tarea

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,177 @@
+using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+using QueryApi.Repositories;
+using QueryApi.Domain;
+
+namespace Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        // Todos los datos //
+
+        [HttpGet]
+        [Route("DatosGenerales")]
+        public IActionResult DatosGenerales ()
+        {
+            var repository = new PersonRepository();
+            var persons = repository.Todalainfo();
+            return Ok(persons);
+        } 
+
+        // Informacion Especifica //
+
+        [HttpGet]
+        [Route("DatosEspecificios")]
+        public IActionResult DatosEspecificos ()
+        {
+            var repository = new PersonRepository();
+            var persons = repository.InformacionEspecifica();
+            return Ok(persons);
+        } 
+
+        // Informacion de Generos //
+        
+        [HttpGet]
+        [Route("FiltrodeGeneros")]
+        public IActionResult FiltradodeGeneros (char genero)
+        {
+            var repository = new PersonRepository();
+            var persons = repository.Infomraciondegenero(genero);
+            return Ok(persons);
+        } 
+
+        // Inforamcion de rango de edades //
+
+        [HttpGet]
+        [Route("RangodeEdades")]
+        public IActionResult RangodeEdades (int edadmin, int edadmax)
+        {
+            var repository = new PersonRepository();
+            var persons = repository.InforamcionRangodeEdades(edadmin, edadmax);
+            return Ok(persons);
+        }
+
+        // Informacion de Trabajos //
+
+        [HttpGet]
+        [Route("TodoslosTrabajos")]
+        public IActionResult InfodeTrabajos ()
+        {
+            var repository = new PersonRepository();
+            var persons = repository.TodoslosTrabajos();
+            return Ok(persons);
+        } 
+
+        // Buscador de letras clave //
+
+        [HttpGet]
+        [Route("LetraClave")]
+        public IActionResult InfoClave (string letraclave)
+        {
+            var repository = new PersonRepository();
+            var persons = repository.InfoClave(letraclave);
+            return Ok(persons);
+        }
+
+        // Buscador de multiples edades //
+        [HttpGet]
+        [Route("EdadesMultiples")]
+        public IActionResult BuscadorMultipledeEdades (int dato1, int dato2, int dato3)
+        {
+            var repository = new PersonRepository();
+            var persons = repository.MultiplesEdades(dato1, dato2, dato3);
+            return Ok(persons);
+        } 
+
+        // Obtencion de informacion desde un punto //
+
+        [HttpGet]
+        [Route("InformacionOrdenadaSegunUnPunto")]
+        public IActionResult BuscarPersonasOrdenadas (int edad)
+        {
+            var repository = new PersonRepository();
+            var persons = repository.Edadesdespuesdeunpunto(edad);
+            return Ok(persons);
+        }
+
+        // Buscador de Genero y Edad //
+
+        [HttpGet]
+        [Route("FiltradodeGeneroyRangodeEdad")]
+        public IActionResult InformacionRangoEdadyGenero (int edadmin, int edadmax, char genero)
+        {
+            var repository = new PersonRepository();
+            var persons = repository.ObtenciondeEdadyRango(edadmin, edadmax, genero);
+            return Ok(persons);
+        } 
+
+        // Contador de Genero //
+
+        [HttpGet]
+        [Route("ConteoDeGenero")]
+        public IActionResult ListadeGenero (char genero)
+        {
+            var repository = new PersonRepository();
+            var persons = repository.ListaseparadadeGenero(genero);
+            return Ok(persons);
+        }
+
+        // Buscador de Apellidos //
+
+        [HttpGet]
+        [Route("ExistenciaDeApellido")]
+        public IActionResult BuscadordeApellido (string apellido)
+        {
+            var repository = new PersonRepository();
+            var persons = repository.ExistenciadeValores(apellido);
+            return Ok(persons);
+        }
+
+        // Filtro de trabajo y edad //
+
+        [HttpGet]
+        [Route("InformacionDeTrabajoyEdad")]
+        public IActionResult InformacionEdadyTrabajo (string trabajo, int edad)
+        {
+            var repository = new PersonRepository();
+            var persons = repository.FiltroTrabajoyEdad(trabajo, edad);
+            return Ok(persons);
+        } 
+
+        // Buscador de los 3 primero resultados //
+
+        [HttpGet]
+        [Route("Los3PrimerosResultados")]
+        public IActionResult Obtener3Personas (string trabajo, int obtencion)
+        {
+            var repository = new PersonRepository();
+            var persons = repository.ObtenerPrimeros3(trabajo, obtencion);
+            return Ok(persons);
+        }
+
+        // Buscador que omita los 3 primeros resultados y otorge los 3 siguientes //
+
+        [HttpGet]
+        [Route("SaltoDeLosPrimeros3")]
+        public IActionResult SaltadorDePersonas (string trabajo, int obtencionomitida)
+        {
+            var repository = new PersonRepository();
+            var persons = repository.Saltode3Personas(trabajo, obtencionomitida);
+            return Ok(persons);
+        } 
+
+        // Obtencion de informacion de las siguiente 3 personas de las iniciales //
+
+        [HttpGet]
+        [Route("Los3Siguientes")]
+        public IActionResult Salto3personas (string trabajo, int salto, int obtencionomitida)
+        {
+            var repository = new PersonRepository();
+            var persons = repository.Saltode3PersonasyObtenciondedatos(trabajo, salto, obtencionomitida);
+            return Ok(persons);
+        }
+    }
+}
---
+++ Infraestructure/PersonRepository.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.IO;
+using QueryApi.Domain;
+using System.Threading.Tasks;
+
+namespace QueryApi.Repositories
+{
+    public class PersonRepository
+    {
+        List<Person> _persons;
+
+        public PersonRepository()
+        {
+            var fileName = "dummy.data.queries.json";
+            if(File.Exists(fileName))
+            {
+                var json = File.ReadAllText(fileName);
+                _persons = JsonSerializer.Deserialize<IEnumerable<Person>>(json).ToList();
+            }
+        }
+
+        // Obtencion de toda la informacion //
+
+        public IEnumerable<Person> Todalainfo ()
+        {
+            return _persons;
+        }
+
+        // Obtencion de informacion especifica //
+
+        public IEnumerable<Object> InformacionEspecifica ()
+        {
+            var query = _persons.Select(person => new {
+
+                NombreCompleto = $"{person.FirstName} {person.LastName}",
+                AnioNacimiento = DateTime.Now.AddYears(person.Age * -1).Year,
+                CorreoElcetronico = person.Email 
+            });
+            
+            return query;
+        }
+
+        // Obtencion de informacion de generos //
+
+        public IEnumerable<Person> Infomraciondegenero (char genero)
+        {
+            var query = _persons.Where(person => person.Gender == genero);
+            return query;
+        }
+
+        // Obtencion de valores de un rango //
+        
+        public IEnumerable<Person> InforamcionRangodeEdades (int edadmin, int edadmax)
+        {
+            var query = _persons.Where(person => person.Age > edadmin && person.Age < edadmax);
+            return query;
+        }
+
+        // Obtencion de informacion de trabajos //
+        
+        public IEnumerable<string> TodoslosTrabajos()
+        {
+            var query = _persons.Select(person => person.Job).Distinct();
+            return query;
+        } 
+
+        // Obtencion de informacion con palabras clave //
+        
+        public IEnumerable<Person> InfoClave (string letraclave)
+        {
+            var query = _persons.Where(person => person.FirstName.Contains(letraclave));
+            return query;
+        }
+
+        // Obtencion de varias edades //
+
+        public IEnumerable<Person> MultiplesEdades (int dato1, int dato2, int dato3)
+        {
+            var edades = new List<int>{dato1,dato2,dato3};
+            var query = _persons.Where(person => edades.Contains(person.Age));
+            return query;
+        }
+
+        // Obtencion de informacion apartir de un punto //
+        
+        public IEnumerable<Person> Edadesdespuesdeunpunto (int edad)
+        {
+            var query = _persons.Where(person => person.Age > edad).OrderBy(person => person.Age);
+            return query;
+        }
+
+        // Obtencion de informacion por genero y rango de edades //
+
+        public IEnumerable<Person> ObtenciondeEdadyRango (int rangoMin, int rangoMax, char genero)
+        {
+            var query = _persons.Where(person => person.Age > rangoMin && person.Age < rangoMax &&  person.Gender ==  genero).OrderByDescending(person =>  person.Gender);
+            return query;
+        }
+
+        // Contador de genero //
+        
+        public IEnumerable<Person> ListaseparadadeGenero (char genero)
+        {
+            var query = _persons.Where(person => person.Gender == genero);
+            return query;
+        }
+
+        // Buscador de existencia //
+
+        public bool ExistenciadeValores (string apellido)
+        {
+            var query = _persons.Any(p => p.LastName == apellido);
+
+            return query;
+        }
+
+        // Filtro de informacion de trabajo y Edad //
+
+        public IEnumerable<Person> FiltroTrabajoyEdad (string trabajo, int edad)
+        {
+            var query = _persons.Where(person => person.Job == trabajo && person.Age == edad);
+            return query;
+        }
+
+        // Buscador de los 3 primeros resultados //
+        
+        public IEnumerable<Person> ObtenerPrimeros3 (string trabajo, int obtencion)
+        {
+            var query = _persons.Where(person => person.Job == trabajo).Take(obtencion);
+            return query;
+        }
+
+        // Buscador que omita los 3 primeros resultados y otorge los 3 siguientes //
+        public IEnumerable<Person> Saltode3Personas (string trabajo, int obtencionomitida)
+        {
+
+            var query = _persons.Where(person => person.Job == trabajo).TakeLast(obtencionomitida);
+            return query;
+        }
+
+        // Obtencion de informacion de las siguiente 3 personas de las iniciales //
+        
+        public IEnumerable<Person> Saltode3PersonasyObtenciondedatos(string trabajo, int salto, int obtencionomitida)
+        {
+            var query = _persons.Where(person => person.Job == trabajo).Skip(salto).Take(obtencionomitida);
+            return query;
+        }
+    }
+}
